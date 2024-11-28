bac7583b01853b27cf774df7add1bdfd3df9f115
Pavel Artemev <damention@MacBook-Pro.local>
2024-11-28 14:39:31 +0100

    testing queryparam

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -10,6 +10,14 @@ public class HelloWorld {
         });
         // Описываем, что загрузится по адресу /
         app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/hello", ctx -> {
+            var name = ctx.queryParam("name");
+            if (name == null) {
+                ctx.result("Hello World");
+            } else {
+                ctx.result("Hello " + name);
+            }
+        });
         app.start(7070); // Стартуем веб-сервер
     }
 }
